70856a90dba5fec4bd6cb5be489a8811223a8b93
rana-mamdouh <ranaammamdouh@gmail.com>
2022-07-01 01:19:33 +0200

    Fixed register problem

--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -39,7 +39,6 @@ export class SignUpComponent implements OnInit {
       this.global.register(this.registerationForm.value).subscribe(res => {
         this.router.navigateByUrl("login")
       }, (err) => {
-        console.log(err)
         this.errorMsg = true
       }, () => {
 
